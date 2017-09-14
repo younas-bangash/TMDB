79455709095ccca7efd0c57520dcdab99c2459cf
YounasBangash <engr.younasbangash@gmail.com>
2017-09-15 00:06:33 +0400

    Fresco API Implementation

--- app/src/main/java/com/tmdb/fragments/RViewAdapter.java
+++ app/src/main/java/com/tmdb/fragments/RViewAdapter.java
@@ -1,15 +1,23 @@
 package com.tmdb.fragments;
 
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.generic.RoundingParams;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.tmdb.R;
 import com.tmdb.databinding.FragmentItemBinding;
 import com.tmdb.interfaces.OnMovieClickListner;
 import com.tmdb.models.MovieDetails;
+import com.tmdb.utils.CircleProgressDrawable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +32,7 @@ public class RViewAdapter extends RecyclerView.Adapter<RViewAdapter.ViewHolder> 
     private List<MovieDetails> movieDetails = new ArrayList<>();
     private final OnMovieClickListner mListener;
     private FragmentItemBinding fragmentItemBinding;
+    private String posterUrl = "http://image.tmdb.org/t/p/w185";
 
     public RViewAdapter(List<MovieDetails> items, OnMovieClickListner listener) {
         movieDetails = items;
@@ -37,11 +46,33 @@ public class RViewAdapter extends RecyclerView.Adapter<RViewAdapter.ViewHolder> 
         return new ViewHolder(fragmentItemBinding);
     }
 
+    public void loadMoviePoster(String posterUrl){
+        fragmentItemBinding.posterImageView.getHierarchy().setProgressBarImage(
+                new CircleProgressDrawable());
+        fragmentItemBinding.posterImageView.setImageURI(posterUrl);
+    }
+
+    public static void loadImageCircle(String url, SimpleDraweeView targetView){
+        ImageRequest request = ImageRequestBuilder.newBuilderWithSource(Uri.parse(url))
+                        .setProgressiveRenderingEnabled(false)
+                        .build();
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                .setImageRequest(request)
+                .setAutoPlayAnimations(false)
+                .build();
+
+        RoundingParams roundingParams = new RoundingParams();
+        roundingParams.setRoundAsCircle(true);
+        targetView.getHierarchy().setRoundingParams(roundingParams);
+
+        targetView.setController(controller);
+    }
+
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = movieDetails.get(position);
         fragmentItemBinding = holder.getViewDataBinding();
-
+        loadMoviePoster("http://image.tmdb.org/t/p/w185"+holder.mItem.posterPath);
         fragmentItemBinding.getRoot().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
